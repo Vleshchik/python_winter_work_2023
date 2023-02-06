efbc673e25f983c23a12caaa615211f2cdd18811
Leshchik_Vadim <vleshchik@gmail.com>
2023-02-06 18:49:14 +0300

    Tasks 8_2 fixed

--- Tasks/Task_8/Task8_2.py
+++ Tasks/Task_8/Task8_2.py
@@ -6,7 +6,7 @@ for i in range(n):
     for j in range(m):
         x = int(input(f'Введите {(j + 1)} число: '))
         b.append(x)
-    a.insert(i,b)
+    a.insert(i, b)
 for i in range(len(a)):
     a[i] = sorted(a[i], reverse= True)
-print(sorted(a, key = lambda x: (x.count(list(x)))))
+print(sorted(a, key = lambda x: len(str(x))))
