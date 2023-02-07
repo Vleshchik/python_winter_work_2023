c4901d9396c6c0a6d24b10d606bdf561e20c97ec
Leshchik_Vadim <vleshchik@gmail.com>
2023-02-08 00:11:03 +0300

    Task 9-3 fixed

--- Tasks/Task_9/Task9_3.py
+++ Tasks/Task_9/Task9_3.py
@@ -1,10 +1,17 @@
 f = open("test1.txt", "r", encoding="UTF-8")
-text = str(f.readlines()).lower()
-tes = set(text)
+text = []
+for line in f:
+        line = line.strip().lower()
+        text.append(line)
+tes = set()
+for i in text:
+    for j in i:
+        tes.add(j)
 dic = {}.fromkeys(tes, 0)
 for a in text:
     for i in a:
-        dic[i] += 1
+        if i in tes:
+            dic[i] += 1
 d = dict(sorted(dic.items(), key=lambda x: (-x[1], x[0])))
 d = dict(list(d.items())[:10])
 for k, v in d.items():
