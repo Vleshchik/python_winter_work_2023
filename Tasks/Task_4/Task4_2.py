03c92abadbecc430c6ea287819ba7ee7c71dafcd
Leshchik_Vadim <vleshchik@gmail.com>
2023-02-14 19:11:30 +0300

    Task 4_2 uploaded

--- Tasks/Task_4/Task4_2.py
+++ Tasks/Task_4/Task4_2.py
@@ -1,26 +1,25 @@
-n = int(input()) # вводим число
+n = int(input())
 a = [0] * n
 for i in range(n):
     a[i] = [0] * n
-# создали пустой массив из n*n элементов
-c = 1 # установили начальное значение
-s = 0 # установили начальное положение точки (старт)
+c = 1
+s = 0
 for s in range(n // 2):
     j = n - s * 2 - 1
-    for x in range(j): # заполняем верхнюю горизонтальную полосу
+    for x in range(j):
         a[s][s + x] = c
         c += 1
-    for y in range(j): # заполняем правую вертикальную полосу
+    for y in range(j):
         a[s + y][n - 1 - s] = c
         c += 1
-    for x in range(j): # заполняем нижнюю горизонтальную полосу
+    for x in range(j):
         a[n - 1 - s][n - 1 - s - x] = c
         c += 1
-    for y in range(j): # заполняем левую вертикальную полосу
+    for y in range(j):
         a[n - 1 - s - y][s] = c
         c += 1
     s += 1
 if n % 2 != 0:
-    a[s][s] = c # вводим последний элемент, если вводимое число - нечетное
+    a[s][s] = c
 for y in range(n):
-    print(a[y]) # выводим итоговый массив
+    print(a[y])
