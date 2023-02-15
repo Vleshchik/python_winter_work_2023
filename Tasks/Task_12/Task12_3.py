c2a64bf5524d6e4c9b060718360a205219fb7cb3
Leshchik_Vadim <vleshchik@gmail.com>
2023-02-15 08:32:16 +0300

    Task 12_3 fixed & uploaded

--- Tasks/Task_12/Task12_3.py
+++ Tasks/Task_12/Task12_3.py
@@ -1,11 +1,11 @@
 lst = [str(x) for x in input().split(', ')] #1-2, 4-4, 3-6
+
 def diapazon(lst):
     res = []
     for i in lst:
         print(i)
-        n1 = int(i[0])
-        n2 = int(i[2])
-        print(i[3])
+        n1 = int(i.split('-')[0])
+        n2 = int(i.split('-')[1])
         while n1 <= n2:
             res.append(n1)
             n1 += 1
