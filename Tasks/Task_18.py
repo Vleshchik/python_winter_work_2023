51b22f0087b7e1d19d1fa49c80b8de7010d94d4a
Leshchik_Vadim <vleshchik@gmail.com>
2023-03-03 14:42:29 +0300

    Tasks 18 in process

--- Tasks/Task_18.py
+++ Tasks/Task_18.py
@@ -1,3 +1,30 @@
+# Класс Урок
+class Lesson:
+    def __init__(self, *lessons):
+        self.lessons = list(lessons)
+    def __getitem__(self, i):
+        return self.lessons[i]
+
+# Класс Задача
+class Task(Lesson):
+    def __init__(self):
+        self.homework = {}
+    def add_task(self):
+        t = int(input('Введите количество задач: '))
+        for i in range(1, t + 1):
+            task = input('Введите название задачи: ')
+            self.tasks.append(task)
+        self.homework = dict.fromkeys(self.tasks, "not solved")
+        return self.homework
+    # Метод для проверки задачи ученика
+    def check_task(self):
+        for k, v in solwed_hv:
+            if v == 'solved':
+                v = 'confirmed'
+            else:
+                continue
+        return (print(solved_hw))
+
 # Класс Преподаватель
 class Teacher(Task):
     def __init__(self, name):
@@ -12,13 +39,13 @@ class Teacher(Task):
             self.work += 1
 
 # Класс Ученик
-class Pupil(Teacher):
+class Pupil(Task):
     def __init__(self, name):
         self.name = name
-        self.homework = homework
+        self.homework = Task.homework
     # Метод для решения задачи
-    def solve_task(self):
-        for k, v in self.homework:
+    def solve_task(self, homework):
+        for k, v in homework:
             res = input(f'Решена ли {k} (Введите "Yes" или "No"): ')
             if res == 'Yes':
                 v = 'solved'
@@ -26,32 +53,13 @@ class Pupil(Teacher):
                 continue
         solved_hw = homework
         return print(solved_hw)
-# Класс Урок
-class Lesson:
-    def __init__(self, *lessons):
-        self.lessons = list(lessons)
-    def __getitem__(self, i):
-        return self.lessons[i]
 
-# Класс Задача
-class Task(Teacher):
-    def add_task(self):
-        t = int(input('Введите количество задач: '))
-        for i in range(1, t +1):
-            task = input('Введите название задачи: ')
-            self.tasks.append(task)
-        homework = dict.fromkeys(self.tasks, "not solved")
-        return print(homework)
-    # Метод для проверки задачи ученика
-    def check_task(self):
-        for k, v in solwed_hv:
-            if v = 'solved':
-                v = 'confirmed'
-            else:
-                continue
-        return (print(solved_hw))
+
+
+
 
 MI = Teacher('МарьИванна')
 Petya = Pupil('Петя')
 MI.add_task()
 Petya.solve_task()
+MI.check_task()
