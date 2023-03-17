8395f6acc7b6309212a2539fc349d47edabd6ec8
Leshchik_Vadim <vleshchik@gmail.com>
2023-03-17 12:08:58 +0300

    Tasks 23 uploaded

--- Tasks/Task_23/Task23_3.py
+++ Tasks/Task_23/Task23_3.py
@@ -1,21 +1,16 @@
 #[1, 4, 21, 3, 43]  [9, 81, 25] 1 4 21 3 43 12
-s = [1, 4, 21, 3, 43, 15, 10, 45]  #[11, 12, 13, 14, 15]
+s = [1, 4, 21, 3, 43, 15, 10, 494, 45]  #[11, 12, 13, 14, 15]
 w = ''
-leng = max(sorted(s, key= lambda x: len(str(x))))
 s.sort(key= lambda x: (str(x)[0], -len(str(x))), reverse=True)
+print(s)
+for i in range(len(s)-1, 0, -1):
+    for x in str(s[i-1]):
+        for y in str(s[i]):
+            if int(x) < int(y):
+                s[i], s[i - 1] = s[i - 1], s[i]
+                continue
 
-for i_0 in range(len(s)-1, 0, -1):
-    for i in range(len(s)-1, 0, -1):
-        if str(s[i])[0] == str(s[i-1])[0]:
-
-            if len(str(s[i - 1])) < len(str(s[i])):
-                n = int(str(s[i])[1:])
-                if s[i-1] < n:
-                    s[i], s[i - 1] = s[i - 1], s[i]
-
-            elif len(str(s[i - 1])) == len(str(s[i])):
-                if s[i] > s[i - 1]:
-                    s[i], s[i - 1] = s[i - 1], s[i]
+print(s)
 for i in s:
     w += str(i)
 print(w)
